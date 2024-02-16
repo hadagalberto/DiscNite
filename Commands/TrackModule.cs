447c5144fd6d2458f455f96276930ba14a5385f1
--unset <--unset>
2024-02-16 00:56:52 -0300

    teste

--- Commands/TrackModule.cs
+++ Commands/TrackModule.cs
@@ -1,5 +1,6 @@
 ﻿using DiscNite.Data;
 using DiscNite.Services;
+using Discord;
 using Discord.Interactions;
 using Fortnite_API.Objects.V1;
 using Microsoft.EntityFrameworkCore;
@@ -336,6 +337,74 @@ namespace DiscNite.Commands
             {
                 await RespondAsync("Ocorreu um erro ao processar a solicitação ❌");
             }
+        }
+
+        [SlashCommand("shop", "Mostra a loja atual do Fortnite")]
+        public async Task ShopAsync()
+        {
+            var shop = await _fortniteApiService.GetShopAsync();
+
+            var itensToShow = shop.Daily.Entries.Take(5);
+
+            var builder = new ComponentBuilder()
+                .WithButton("Próxima página", "next", ButtonStyle.Primary)
+                .WithButton("Página anterior", "previous", ButtonStyle.Primary);
+
+            var embed = new EmbedBuilder()
+                .WithTitle("Loja do Fortnite")
+                .WithDescription("Aqui estão os itens da loja atual do Fortnite")
+                .WithColor(Color.Blue)
+                .WithImageUrl(itensToShow.FirstOrDefault().Items.FirstOrDefault().Images.Featured.ToString())
+                .WithFooter("Loja atual do Fortnite")
+                .Build();
+
+            await RespondAsync(embeds: new[] { embed }, components: builder.Build());
+        }
+
+        [SlashCommand("ping", "Pong!")]
+        public async Task PingAsync()
+        {
+            await RespondAsync("Pong!");
+        }
+
+        [SlashCommand("info", "Mostra informações sobre o bot")]
+        public async Task InfoAsync()
+        {
+            var description = "DiscNite é um bot para o Discord que fornece informações sobre o Fortnite";
+
+            description += "\n\n";
+
+            description += "Ele pode mostrar a loja atual do Fortnite, estatísticas de jogadores e muito mais!";
+
+            description += "\n\n";
+
+            description += "Para ver todos os comandos disponíveis, digite /help";
+
+            var embed = new EmbedBuilder()
+                .WithTitle("DiscNite")
+                .WithDescription("DiscNite é um bot para o Discord que fornece informações sobre o Fortnite")
+                .WithColor(Color.Blue)
+                .WithFooter("DiscNite")
+                .Build();
+
+            await RespondAsync(embeds: new[] { embed });
+        }
+
+        [SlashCommand("help", "Mostra todos os comandos disponíveis")]
+        public async Task HelpAsync()
+        {
+            var builder = new ComponentBuilder()
+                .WithButton("Loja", "shop", ButtonStyle.Primary)
+                .WithButton("Estatísticas", "stats", ButtonStyle.Primary);
+
+            var embed = new EmbedBuilder()
+                .WithTitle("Comandos disponíveis")
+                .WithDescription("Aqui estão todos os comandos disponíveis")
+                .WithColor(Color.Blue)
+                .WithFooter("Comandos disponíveis")
+                .Build();
+
+            await RespondAsync(embeds: new[] { embed }, components: builder.Build());
         }
 
     }
